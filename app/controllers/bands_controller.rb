1bcd2cbd2bee73340888e9b07b6141af789e76ee
chipmstr <dan.ursu@hotmail.com>
2024-10-26 12:43:23 +0200

    bands_controller

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -12,11 +12,20 @@ class BandsController < ApplicationController
   end
 
   def edit
+    @band = Band.find(params[:id])
   end
 
   def update
+    if @band.update(band_params)
+      redirect_to @band, notice: 'Band was successfully updated.'
+    else
+      render :edit, alert: 'Band was not updated.'
+    end
   end
 
   def destroy
+    @band = Band.find(params[:id])
+    @band.destroy
+    redirect_to_bands_path, notice: 'Band was successfully deleted.'
   end
 end
