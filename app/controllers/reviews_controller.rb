a88bc1036cc041b72b3766581d5368fcb5c26315
matteo trianni <mtrianni26@gmail.com>
2024-10-26 23:38:38 +0200

    created empty sessions and applications controller, and filled out user one and review one

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,22 +1,22 @@
 class ReviewsController < ApplicationController
-  def index
-  end
-
-  def show
-  end
-
-  def new
-  end
 
   def create
-  end
-
-  def edit
-  end
-
-  def update
+    @band = Band.find(params[:band_id])
+    @review = @band.build_review(review_params)
+    if @review.save
+      redirect_to band_path(@band), notice: 'Review successfully created'
+    else
+      render 'band#show', status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @review = Review.find(params[:id])
+    if @review.user == current_user
+      @review.destroy
+      redirect_to @review.band, notice: 'Review was successfully deleted'
+    else
+       redirect_to @review.band, alert: 'You are not authorized to delete this review.'
+    end
   end
 end
