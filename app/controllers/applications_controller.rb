562c89de64dea878ebc9a2510b3f01dbd28893f5
matteo trianni <mtrianni26@gmail.com>
2024-10-27 00:19:21 +0200

    implemented sessionsController and applicationsController

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,8 +1,46 @@
 class ApplicationsController < ApplicationController
+  before_action :set_application, only: [:destroy]
+  before_action :set_session, only: [:create]
+  before_action :set_band, only: [:create]
 
   def create
+    @application = Application.new
+    @application.session_id = @session.id
+    @application.band_id = @band.id
+    @application.user_id = current_user.id
+    @application.status = "pending"
+
+    if @application.save
+      redirect_to session_path(@session), notice: 'Application was successfully created.'
+      @application.status = 'completed'
+    else
+      redirect_to session_path(@session), alert: 'Failed to create application.'
+    end
   end
 
   def destroy
+    if @application.destroy
+      redirect_to session_path(@application.session), notice: 'Application was successfully deleted.'
+    else
+      redirect_to session_path(@application.session), alert: 'Failed to delete application.'
+    end
+  end
+
+  private
+
+  def set_application
+    @application = Application.find(params[:id])
+  end
+
+  def set_session
+    @session = Session.find(params[:session_id])
+  end
+
+  def set_band
+    @band = Band.find(params[:band_id])
+  end
+
+  def application_params
+    params.require(:application).permit(:status)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,19 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update destroy]
+
   def index
     @users = User.where.not(id: current_user.id)
   end
 
   def show
-    @user = User.find(params[:id])
+
   end
 
   def edit
-    @user = User.find(params[:id])
+
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to @user, notice: 'User was successfully updated.'
     else
@@ -21,7 +22,6 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id])
     if @user.id = current_user.id
       @user.destroy
       redirect_to 'bands#index', notice: 'Your account was successfully deleted.'
@@ -31,7 +31,9 @@ class UsersController < ApplicationController
   end
 
   private
-
+  def set_user
+    @user = User.find(params[:id])
+  end
   def user_params
     params.require(:user).permit(:first_name, :last_name, :instruments, :address, :email, :telephone_number)
   end
