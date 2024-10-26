1b52ccf621df27c90990766aa5ebba19e969aff8
matteo trianni <mtrianni26@gmail.com>
2024-10-26 11:38:08 +0200

    fixed a comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   belongs_to :band, optional: true
-  # has_many :chats
-  # has_many :messages
-  # has_many :reviews
+   has_many :chats
+   has_many :messages
+   has_many :reviews
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
