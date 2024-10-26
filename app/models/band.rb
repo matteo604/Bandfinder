14ce9a3806993a85aa1db5691351f47805cf8877
matteo trianni <mtrianni26@gmail.com>
2024-10-26 12:26:43 +0200

    half band controller is set

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,5 +1,6 @@
 class Band < ApplicationRecord
-  # has_many :reviews, :users
+  has_many :reviews
+  has_many :users
   belongs_to :band_leader, class_name: "User"
 
   validates :title, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_26_091044) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_26_102551) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,7 +30,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_26_091044) do
     t.string "title", null: false
     t.string "address", null: false
     t.string "genre"
-    t.integer "min_member"
     t.text "description"
     t.float "latitude"
     t.float "longitude"
