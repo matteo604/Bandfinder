d6f662de80969f3f8db295162638877a4facda02
chipmstr <dan.ursu@hotmail.com>
2024-11-02 06:55:38 +0100

    fixed chat issues, schema adjustments, display current user in navbar

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,6 +1,5 @@
 class Chat < ApplicationRecord
   belongs_to :user
-  belongs_to :band
-  belongs_to :band_leader, class_name: "User", foreign_key: "band_leader_id" # Associate band_leader_id with User
+  belongs_to :band_leader, class_name: "User", foreign_key: "band_leader_id" # associate band_leader_id with user
   has_many :messages, dependent: :destroy
 end
