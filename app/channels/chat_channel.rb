84b67496210feeec8a6d66d30472b95e2df16408
matteo trianni <mtrianni26@gmail.com>
2024-11-07 18:57:47 +0100

    removed chat_channel file

--- app/channels/chat_channel.rb
+++
@@ -1,9 +0,0 @@
-class ChatChannel < ApplicationCable::Channel
-  def subscribed
-    stream_from "chat_#{params[:chat_id]}"
-  end
-
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-  end
-end
