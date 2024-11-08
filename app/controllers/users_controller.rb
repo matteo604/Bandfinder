da3b2bbe174dce9239d5299416e7caaee3a72187
matteo trianni <mtrianni26@gmail.com>
2024-11-08 15:36:24 +0100

    filled about us page (for me and Dan)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,16 +11,30 @@ class UsersController < ApplicationController
   end
 
   def edit
-
   end
 
   def update
-    if params[:user][:instruments].present?
-      @user.instruments = params[:user][:instruments].split(',').map(&:strip) # Convert comma-separated string into an array
+    # Debug: verifica che params[:user] sia presente e abbia la chiave :instruments
+    Rails.logger.debug("User params: #{params[:user].inspect}")
+
+    # Assicurati che instruments sia un array e manipola i dati
+    if params[:user][:instruments].is_a?(Array)
+      Rails.logger.debug("Initial instruments array: #{params[:user][:instruments].inspect}")
+
+      # Rimuovi valori vuoti e spazi bianchi
+      non_blank_instruments = params[:user][:instruments].reject(&:blank?).map(&:strip)
+      Rails.logger.debug("Non-blank instruments array after strip: #{non_blank_instruments.inspect}")
+
+      # Unisci gli strumenti in una stringa
+      params[:user][:instruments] = non_blank_instruments.join(", ")
+      Rails.logger.debug("Final instruments string: #{params[:user][:instruments]}")
     end
+
+    # Verifica se l'aggiornamento del user va a buon fine
     if @user.update(user_params)
       redirect_to @user, notice: 'User was successfully updated.'
     else
+      Rails.logger.debug("User update failed: #{@user.errors.full_messages.inspect}")
       render :edit
     end
   end
@@ -39,8 +53,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  private
+
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :telephone_number, :address, instruments: [])
+    params.require(:user).permit(:first_name, :last_name, :email, :telephone_number, :address, :instruments)
   end
 
 end
